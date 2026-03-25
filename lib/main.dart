9c9b89d03e09cfb91104da6e57287e9166b34bce
gasare <gasarejoel@gmail.com>
2026-03-25 13:34:26 +0200

    fix: resolve Undefined name 'WidgetsFlutterBinding' by adding explicit import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
@@ -6,14 +8,14 @@ import 'core/storage/hive_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Hive
   await Hive.initFlutter();
-  
+
   // Initialize secure storage
   final hiveService = HiveService();
   await hiveService.init();
-  
+
   runApp(
     ProviderScope(
       overrides: [
